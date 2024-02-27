44d5e739685b9e1c5881f073b1bf43efadeb72e0
Run Huang <19307110301@fudan.edu.cn>
2024-02-26 21:49:52 -0800

    add join us section

---
+++ app/Section/opportunities.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Section from "../UI/section";
+import { alegreya } from "../UI/font";
+import styles from "@/styles/opportunity.module.css";
+import Link from "next/link";
+
+export default function Opportunities() {
+  return (
+    <Section
+      id="opportunities"
+      title="Join us"
+      icon={
+        <Image
+          src="/icon/ace.svg"
+          alt="Ace opportunities"
+          width={30}
+          height={30}
+          layout="fixed"
+        />
+      }
+      className={alegreya.className}
+    >
+      <div className={styles.opp_container}>
+        We are currently <span className={styles.highlight}>NOT hiring</span>{" "}
+        (sorry!). We encourage you to check back in a few months for updates.
+        Still, if you have a genuine interest in the{" "}
+        <a href="#project" className={styles.highlight}>
+          projects
+        </a>{" "}
+        listed above and have unique insights to share, we invite you to
+        directly contact the project leads. We *might* be able to work something
+        out :)
+        <p
+          style={{ fontSize: "1.2rem", marginTop: "0.6rem", color: "#5a5a5a" }}
+        >
+          <i>Please note:</i> it is recommended{" "}
+          <span className={styles.highlight}>NOT</span> to email Prof.
+          Chattopadhyay directly as she is unable to respond promptly, owing to
+          the high volume of emails she receives. Instead, please reach out to
+          individual project leads.
+        </p>
+        {/* <div className={styles.opp_button__container}>
+          <Link
+            href="https://forms.gle/4dUU8nxKzYyzWBWk6"
+            className={styles.opp_disabled_button}
+          >
+            For Current Students at USC
+          </Link>
+          <Link
+            href="https://forms.gle/uhygFA411NaY4Rvj7"
+            className={styles.opp_disabled_button}
+          >
+            For Prospective Graduate Students
+          </Link>
+        </div> */}
+      </div>
+    </Section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "./Section/hero";
 import Projects from "./Section/projects";
 import Publications from "./Section/publications";
 import people_data from "@/app/Data/people.json";
+import Opportunities from "./Section/opportunities";
 
 const current_member = people_data.filter((person) => !person.alumni);
 const alumni = people_data.filter((person) => person.alumni);
@@ -14,6 +15,7 @@ export default function Home() {
       <Hero />
       <People id="team" title="our team" data={current_member} />
       <Projects />
+      <Opportunities />
       <Publications />
       <People id="alumni" title="ace alumni" data={alumni} small />
     </Fragment>
