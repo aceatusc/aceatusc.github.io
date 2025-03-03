cf42429fdb7bcc5ee1858047cd196da42db22b45
Run Huang <runhuang@usc.edu>
2025-03-03 14:12:17 -0800

    fix bugs, updates

--- app/UI/footer.tsx
+++ app/UI/footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className={`${alegreya.className} ${styles.footer_container}`}>
-      <div style={{ marginRight: "2.4rem" }}>Last updated: Sep 2024</div>
+      <div style={{ marginRight: "2.4rem" }}>Last updated: Mar 2025</div>
       <div>
         © Copyright 2024 ACE Lab. Website developed by{" "}
         <Link
